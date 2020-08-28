169d50d78fffc874af8a32c1409dc6f7f3bbc24c
Jason <jason@casjay.net>
2020-08-28 18:27:38 -0400

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,19 @@
+## template  
+
+requires:
+
+```bash
+sudo bash -c "$(curl -LSs https://github.com/wallpapermgr/template/raw/master/install.sh)"
+```  
+
+install:
+
+```bash
+sudo wallpapermgr install template
+```
+
+update:
+
+```bash
+sudo wallpapermgr update template
+```
---
+++ install.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+APPNAME="template"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @Author          : Jason
+# @Contact         : casjaysdev@casjay.net
+# @File            : install.sh
+# @Created         : Fr, Aug 28, 2020, 00:00 EST
+# @License         : WTFPL
+# @Copyright       : Copyright (c) CasjaysDev
+# @Description     : installer script for Template wallpaper pack
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Set functions
+
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/casjay-dotfiles/scripts/raw/master/functions}"
+SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+elif [ -f "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
+else
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/$SCRIPTSFUNCTFILE"
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+system_installdirs
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Make sure the scripts repo is installed
+
+scripts_check
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Defaults
+APPNAME="${APPNAME:-template}"
+APPDIR="${APPDIR:-$SHARE/CasjaysDev/wallpapermgr}/$APPNAME"
+REPO="${WALLPAPERMGRREPO:-https://github.com/wallpapermgr}/${APPNAME}"
+REPORAW="${REPORAW:-$REPO/raw}"
+APPVERSION="$(curl -LSs $REPORAW/master/version.txt)"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# dfmgr_install fontmgr_install iconmgr_install pkmgr_install systemmgr_install thememgr_install wallpapermgr_install
+
+wallpapermgr_install
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Script options IE: --help
+
+show_optvars "$@"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# end with a space
+
+APP=""
+
+# install packages - useful for package that have the same name on all oses
+install_packages $APP
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Ensure directories exist
+
+ensure_dirs
+ensure_perms
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Main progam
+
+if [ -d "$APPDIR/.git" ]; then
+  execute \
+    "git_update $APPDIR" \
+    "Updating $APPNAME configurations"
+else
+  execute \
+    "backupapp && \
+        git_clone -q $REPO/$APPNAME $APPDIR" \
+    "Installing $APPNAME configurations"
+fi
+
+# exit on fail
+failexitcode
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# run post install scripts
+
+run_postinst() {
+  wallpapermgr_run_post
+}
+
+execute \
+  "run_postinst" \
+  "Running post install scripts"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# create version file
+
+wallpapermgr_install_version
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# exit
+run_exit
+
+# end
